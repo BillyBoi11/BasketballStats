06cbfea5768be8b39da64f8988b409c585ca7b6f
BillyBoi11 <157395247+BillyBoi11@users.noreply.github.com>
2026-02-23 20:18:45 +0100

    Add files via upload

--- database.js
+++ database.js
@@ -2,25 +2,38 @@
 const GAME_DATABASE = [ 
   {
     date: "2026-02-23",
+    gameName: "Game 9",
+    duration: "9:00",
+    reboundGame: false, 
+    scheduled: false,
+    players: [
+      { name: "Lucas", team: "Team 1", twoP: 4, threeP: 1, ft: 0, miss: 0, foul: 3, reb: 0, tech: 0, stl: 1, blk: 0},
+      { name: "Gem", team: "Team 1", twoP: 4, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 0, blk: 0},
+      { name: "Billy", team: "Team 2", twoP: 3, threeP: 1, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 2, blk: 1},
+      { name: "Viking", team: "Team 2", twoP: 4, threeP: 0, ft: 0, miss: 0, foul: 1, reb: 0, tech: 0, stl: 0, blk: 0}
+    ]
+  }, 
+  {
+    date: "2026-02-23",
     gameName: "Game 8",
-    duration: "8:00",
-    reboundGame: true, 
+    duration: "11:00",
+    reboundGame: false, 
     scheduled: false,
     players: [
       { name: "Lucas", team: "Team 1", twoP: 1, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 0, blk: 0},
       { name: "Gem", team: "Team 1", twoP: 3, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 0, blk: 0},
-      { name: "Billy", team: "Team 2", twoP: 1, threeP: 1, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 1, blk: 0},
+      { name: "Billy", team: "Team 2", twoP: 1, threeP: 2, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 1, blk: 0},
       { name: "Viking", team: "Team 2", twoP: 4, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 1, blk: 0}
     ]
   }, 
   {
     date: "2026-02-23",
     gameName: "Rotating 1v2 + ref",
-    duration: "20:00",
+    duration: "40:00",
     reboundGame: true, 
     scheduled: false,
     players: [
-      { name: "Billy", team: "Team 1", twoP: 5, threeP: 1, ft: 0, miss: 0, foul: 1, reb: 10, tech: 0, stl: 2, blk: 0},
+      { name: "Billy", team: "Team 1", twoP: 5, threeP: 1, ft: 0, miss: 0, foul: 1, reb: 11, tech: 0, stl: 3, blk: 0},
       { name: "Lucas", team: "Team 2", twoP: 4, threeP: 0, ft: 0, miss: 0, foul: 2, reb: 1, tech: 0, stl: 0, blk: 0},
       { name: "Gem", team: "Team 3", twoP: 5, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 2, tech: 0, stl: 1, blk: 0},
       { name: "Viking", team: "Team 4", twoP: 7, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 12, tech: 0, stl: 1, blk: 1}
@@ -56,7 +69,7 @@ const GAME_DATABASE = [
   {
     date: "2026-02-10",
     gameName: "Game 6",
-    duration: "7:00",
+    duration: "5:00",
     reboundGame: false, 
     scheduled: false,
     players: [
@@ -83,7 +96,7 @@ const GAME_DATABASE = [
     scheduled: false,
     players: [
       { name: "Billy", team: "Team 1", twoP: 2, threeP: 0, ft: 0, miss: 0, foul: 1, reb: 0, tech: 0, stl: 0, blk: 1},
-      { name: "Gem", team: "Team 2", twoP: 6, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 0, blk: 0},
+      { name: "Gem", team: "Team 2", twoP: 6, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 1, blk: 0},
       { name: "Lucas", team: "Team 2", twoP: 0, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 0, blk: 0}
     ]
   },
@@ -94,7 +107,7 @@ const GAME_DATABASE = [
     reboundGame: false, 
     scheduled: false,
     players: [
-      { name: "Gem", team: "Team 1", twoP: 6, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 0, blk: 0},
+      { name: "Gem", team: "Team 1", twoP: 6, threeP: 0, ft: 0, miss: 0, foul: 0, reb: 0, tech: 0, stl: 1, blk: 0},
       { name: "Billy", team: "Team 2", twoP: 0, threeP: 3, ft: 0, miss: 0, foul: 1, reb: 0, tech: 0, stl: 0, blk: 0},
       { name: "Lucas", team: "Team 2", twoP: 2, threeP: 0, ft: 0, miss: 0, foul: 2, reb: 0, tech: 0, stl: 0, blk: 0}
     ]
@@ -125,4 +138,3 @@ const GAME_DATABASE = [
 
 
 ];
-
