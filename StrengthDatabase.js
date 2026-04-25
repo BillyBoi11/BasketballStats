f34072ebc60fd7c93a48693ad50df87328e80fa0
BillyBoi11 <157395247+BillyBoi11@users.noreply.github.com>
2026-04-25 19:54:11 +0200

    Add files via upload

---
+++ StrengthDatabase.js
@@ -0,0 +1,11 @@
+const STRENGTH_DATABASE = [
+{
+    date: "2026-04-24",
+    sessionName: "Strength Test 1",
+    duration: "45:00",
+    group: "Airball Squad",
+    players: [
+        { name: "Billy", maxPushups: 43, maxPullups: 13, maxDips: 53, maxDiamondPushups: 21, maxBenchPress: 60, maxGroundLift: 90, maxJumpReach: 298 },
+    ]
+},
+];
